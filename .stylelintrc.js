66452b4fb64ac8289a5851121fb6589ca81719c6
hellodword <46193371+hellodword@users.noreply.github.com>
2023-10-16 13:04:06 +0000

    feat(stylelint): init stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+    customSyntax: 'postcss-scss',
+    rules: {
+        'scss/dollar-variable-pattern': null,
+    },
+};

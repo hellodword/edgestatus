3d62e022a50ee3dec3e35df7759315b856a66064
hellodword <46193371+hellodword@users.noreply.github.com>
2023-10-18 11:49:09 +0000

    fix: remove a vite plugin that causes npm/cli#6869

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
-import now from '~build/time';
 import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
+const now = import.meta.env.VITE_APP_BUILD_TIME;
+console.log(now);
+const buildDate = new Date(Number.parseInt(now) * 1000).toLocaleString();
 </script>
 
 <template>
     <v-col class="text-center text-truncate mt-4" cols="12">
-        {{ t('common.buildAt') }} {{ now.toLocaleString() }}
+        {{ t('common.buildAt') }} {{ buildDate }}
     </v-col>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
-import Info from 'vite-plugin-info';
 import Inspect from 'vite-plugin-inspect';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
@@ -15,7 +14,6 @@ export default defineConfig({
     },
     plugins: [
         vue(),
-        Info(),
         Inspect(),
         VueI18nPlugin({
             runtimeOnly: true,
